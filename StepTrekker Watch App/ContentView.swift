ca7bc57b77961134dd50c5cc2f5fa392ab6f4f9e
Ljubisic,Bratislav (BI X) BIX-DE-I <bratislav.ljubisic@boehringer-ingelheim.com>
2024-11-26 20:13:33 +0100

    Added inserting nickName to userDefaults

--- StepTrekker Watch App/ContentView.swift
+++ StepTrekker Watch App/ContentView.swift
@@ -10,14 +10,34 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State var nickName: String
+    @State var nickName: String = UserDefaults.standard.string(forKey: "nickName") ?? ""
+    @State private var editing = false
     
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            TextField("Nickname", text: $nickName)
+            Spacer()
+            if (nickName.isEmpty || editing) {
+                TextField("Nickname", text: $nickName)
+                    .onSubmit {
+                        UserDefaults.standard.set(nickName, forKey: "nickName")
+                        editing = false
+                    }
+            } else {
+                HStack {
+                    Text(nickName)
+                    Spacer()
+                    Button {
+                        editing = true
+                    } label: {
+                        Image(systemName: "pencil")
+                            .imageScale(.large)
+                            .foregroundStyle(.tint)
+                    }
+                }
+            }
             Spacer()
         }
         .padding()
